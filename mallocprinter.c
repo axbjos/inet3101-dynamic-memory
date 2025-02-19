a66c5174b318d8e9abeae70f9b297ee71401717b
Joe Axberg <jaxberg@umn.edu>
2025-02-19 17:43:30 -0600

    removed files not needed

---
+++ mallocprinter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int *array1 = (int *)malloc(4 * sizeof(int));
+
+    printf("\n\nIndex 0 of the memory allocation starts at: %p" , &array1[0]);
+    printf("\n\nIndex 1 of the memory allocation starts at: %p" , &array1[1]);
+
+    printf("\n\nValue at index 0 is %d: ", array1[0]);
+    printf("\n\nValue at index 1 is %d: ", array1[1]);
+
+    printf("\n\n");
+
+}
